bf60ee8832856477479960d088aecffa6a5849bc
Oleh Honchar <hocharol@gmail.com>
2022-05-17 15:03:23 +0300

    ver 0

---
+++ src/olg/stud/LibraryReaders.java
@@ -0,0 +1,15 @@
+package olg.stud;
+
+public class LibraryReaders{
+    public String name;
+    public String libraryCard;
+    public String faculty;
+    public String dateOfBirth;
+    public int phoneNumber;
+
+    public void addReader(String str ){
+
+
+    }
+
+}
---
+++ src/olg/stud/Start.java
@@ -0,0 +1,10 @@
+package olg.stud;
+
+public class Start extends LibraryReaders {
+    public static void main(String[] args) {
+        LibraryReaders libraryReaders = new LibraryReaders();
+        libraryReaders.name = "loco";
+//        name = "loko";
+//        libraryReaders.addReader(name);
+    }
+}
