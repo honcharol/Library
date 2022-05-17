e7738792de4be5dc71ab3655f6d3ed5394f41b6b
Oleh Honchar <hocharol@gmail.com>
2022-05-17 20:23:35 +0300

    ver 1

--- src/olg/stud/Book.java
+++ src/olg/stud/Book.java
@@ -1,14 +1,11 @@
 package olg.stud;
 
 public class Book extends LibraryReaders {
-    Book b = new Book();
 
     String [][] arrStr = {{"book1",   "book2",   "book3",   "book4",   "book5"},
                           {"author1", "author2", "author3", "author4", "author5"}};
-
     public void takeBook(int str ){
-        String s = b.getName();
-        System.out.println( s + "  took  "+ str+ "  books");
+        System.out.println( getName() + "  took  "+ str+ "  books");
     }
     public void takeBook(String str ){
 
